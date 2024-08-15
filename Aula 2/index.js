580ede2b9dfff53473ab7e3edecc19030665248d
Diony Junior Passos Viana <diony.passos@gmail.com>
2024-08-14 21:25:26 -0300

    Primeira aula de JS

---
+++ Aula 2/index.js
@@ -0,0 +1,19 @@
+let nome =  "Diony";
+let sobrenome = "Junior Passos Viana"
+let ano = 2024
+let idade = 25
+
+const nomeCompleto = nome + " " + sobrenome
+
+const calc = ano - idade
+
+console.log(nomeCompleto)
+console.log(calc)
+
+const num1 = parseInt(prompt("Digite um número: "))
+
+const num2 = parseInt(prompt("Digite mais um número: "))
+
+const soma = num1 + num2 
+
+console.log(soma)
